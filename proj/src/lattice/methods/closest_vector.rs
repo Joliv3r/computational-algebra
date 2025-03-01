2d78f14c49e85f51c9341bfb045c7c678934a9cf
Joliver <jacobbruun@hotmail.no>
2025-03-01 23:36:51 +0100

    feat: One mutable vector for tracking combinations in enumerations

--- proj/src/lattice/methods/closest_vector.rs
+++ proj/src/lattice/methods/closest_vector.rs
@@ -44,11 +44,12 @@ impl Lattice {
 
         let mut checked_points = vec![(closest_vector[0], closest_vector[1])];
 
-        let (lower_bound, upper_bound) = self.get_cvp_enumeration_bounds(&Vec::<i64>::new(), 0, shortest_distance, &y);
+        let mut combination = vec![0; self.columns()];
+        let (lower_bound, upper_bound) = self.get_cvp_enumeration_bounds(&combination, 0, shortest_distance, &y);
 
         for x_n in lower_bound..=upper_bound {
-            let combination = vec![x_n];
-            let (candidate_vector, candidate_distance) = self.closest_vector_enumeration_step(vector, 1, &combination, &closest_vector, shortest_distance, &y, &mut checked_points);
+            combination[self.columns()-1] = x_n;
+            let (candidate_vector, candidate_distance) = self.closest_vector_enumeration_step(vector, 1, &mut combination, &closest_vector, shortest_distance, &y, &mut checked_points);
             if candidate_distance < shortest_distance {
                 closest_vector = candidate_vector;
                 shortest_distance = candidate_distance;
@@ -58,10 +59,9 @@ impl Lattice {
         Ok((closest_vector, checked_points))
     }
 
-    fn closest_vector_enumeration_step(&self, vector: &Array1<f64>, depth: usize, combination: &Vec<i64>, current_closest_vector: &Array1<f64>, current_shortest_distance: f64, y: &Array1<f64>, checked_points: &mut Vec<(f64, f64)>) -> (Array1<f64>, f64) {
+    fn closest_vector_enumeration_step(&self, vector: &Array1<f64>, depth: usize, combination: &mut Vec<i64>, current_closest_vector: &Array1<f64>, current_shortest_distance: f64, y: &Array1<f64>, checked_points: &mut Vec<(f64, f64)>) -> (Array1<f64>, f64) {
         if depth == self.columns() {
-            let indexes = &combination.iter().rev().cloned().collect_vec();
-            let current_vector = self.get_lattice_point(indexes).expect("Should exist.");
+            let current_vector = self.get_lattice_point(combination).expect("Should exist.");
             let current_distance = get_length_of_vector(&(vector-&current_vector));
             // println!("Found current distance: {}, with {}", current_distance, current_vector);
             checked_points.push((current_vector[0], current_vector[1]));
@@ -72,12 +72,11 @@ impl Lattice {
         let mut shortest_distance = current_shortest_distance;
         let (lower_bound, upper_bound) = self.get_cvp_enumeration_bounds(combination, depth, shortest_distance, y);
 
-        // println!("Checking {}, with {} .. {}", combination[0], lower_bound, upper_bound);
-
         for i in lower_bound..=upper_bound {
-            let mut new_combination = combination.clone();
-            new_combination.push(i);
-            let (candidate_vector, candidate_distance) = self.closest_vector_enumeration_step(vector, depth+1, &new_combination, &closest_vector, shortest_distance, y, checked_points);
+            // let mut new_combination = combination.clone();
+            // new_combination.push(i);
+            combination[self.columns()-1-depth] = i;
+            let (candidate_vector, candidate_distance) = self.closest_vector_enumeration_step(vector, depth+1, combination, &closest_vector, shortest_distance, y, checked_points);
             if candidate_distance < shortest_distance {
                 closest_vector = candidate_vector;
                 shortest_distance = candidate_distance;
@@ -97,11 +96,11 @@ impl Lattice {
         let end = self.columns();
         let i = self.columns()-basis_number-1;
         // let z = self.write_vector_with_gram_schmidt_vectors_from_reversed_basis_representation(combination);
-        for (x_j,  j) in combination.iter().zip((start..end).into_iter().rev()) {
+        for j in (start..end).into_iter().rev() {
             let B_j = self.get_gram_schmidt_length(j);
             let mu_ji = self.get_mu(j, i);
-            sum += ((self.get_gram_schmidt_coefficient(&combination) - y.get(j).expect("Should exist.")).powf(2.) as f64)*B_j;
-            N_i += mu_ji * (*x_j as f64);
+            sum += ((self.get_gram_schmidt_coefficient(&combination, j) - y.get(j).expect("Should exist.")).powf(2.) as f64)*B_j;
+            N_i += mu_ji * (combination[j] as f64);
         }
         let M_i = ((A - sum)/self.get_gram_schmidt_length(i)).sqrt();
         let y_i = y.get(i).expect("Should exist.");
@@ -115,18 +114,14 @@ impl Lattice {
 
     // This help function is only for get_cvp_enumeration_bounds(...), and will therefore be
     // written to work with this, and not for general purpose.
-    pub fn get_gram_schmidt_coefficient(&self, combination: &Vec<i64>) -> f64 {
+    pub fn get_gram_schmidt_coefficient(&self, combination: &Vec<i64>, i: usize) -> f64 {
         // let num_of_zeros = self.columns()-combination.len();
         // let comb = combination.iter().copied().chain(vec![0; num_of_zeros].iter().copied()).rev().collect();
         // let vector = self.get_lattice_point(&comb).expect("Combination should be valid.");
 
-        let i = self.columns() - combination.len();
-        let mut z_i = 0.;
-        for (j, x_j) in combination.iter().enumerate() {
-            if j == combination.len()-1 {
-                z_i += *x_j as f64;
-            }
-            z_i += (*x_j as f64)*self.get_mu(self.columns()-j-1, i);
+        let mut z_i = combination[i] as f64;
+        for j in i+1..self.columns() {
+            z_i += (combination[j] as f64)*self.get_mu(j, i);
         }
 
         z_i
--- proj/src/lattice/methods/shortest_vector.rs
+++ proj/src/lattice/methods/shortest_vector.rs
@@ -17,13 +17,14 @@ impl Lattice {
         let mut shortest_length = shortest_vector.dot(&shortest_vector);
         let mut checked_points: Vec<(f64, f64)> = vec![(shortest_vector[0], shortest_vector[1])];
 
+        let mut combination = vec![0; self.columns()];
         let bound_n = (shortest_length/self.get_gram_schmidt_length(self.columns()-1)).sqrt().floor() as i64;
 
         println!("Bound on n is {}", bound_n);
 
         for x_n in 0..bound_n {
-            let combination = vec![x_n];
-            let (candidate_vector, candidate_length) = self.shortest_vector_enumeration_steps(1, &combination, &shortest_vector.to_vec().into(), shortest_length, &mut checked_points);
+            combination[self.columns()-1] = x_n;
+            let (candidate_vector, candidate_length) = self.shortest_vector_enumeration_steps(1, &mut combination, &shortest_vector.to_vec().into(), shortest_length, &mut checked_points);
             if candidate_length < shortest_length && candidate_length != 0. {
                 shortest_vector = candidate_vector;
                 shortest_length = candidate_length;
@@ -33,9 +34,9 @@ impl Lattice {
         Ok((shortest_vector, checked_points))
     }
 
-    fn shortest_vector_enumeration_steps(&self, depth: usize, combination: &Vec<i64>, current_shortes_vector: &Array1<f64>, current_shortest_length: f64, checked_points: &mut Vec<(f64, f64)>) -> (Array1<f64>, f64) {
+    fn shortest_vector_enumeration_steps(&self, depth: usize, combination: &mut Vec<i64>, current_shortes_vector: &Array1<f64>, current_shortest_length: f64, checked_points: &mut Vec<(f64, f64)>) -> (Array1<f64>, f64) {
         if depth == self.columns() {
-            let current_vector = self.get_lattice_point(&combination.iter().rev().cloned().collect_vec()).expect("Should exist.");
+            let current_vector = self.get_lattice_point(combination).expect("Should exist.");
             let current_length = current_vector.dot(&current_vector);
             println!("Depth reached.");
             checked_points.push((current_vector[0], current_vector[1]));
@@ -47,9 +48,8 @@ impl Lattice {
         let (lower_bound, upper_bound) = self.get_svp_enumeration_bounds(combination, depth, shortest_length);
         println!("Checking in {}..{}", lower_bound, upper_bound);
         for i in lower_bound..=upper_bound {
-            let mut new_combination = combination.clone();
-            new_combination.push(i);
-            let (candidate_vector, candidate_length) = self.shortest_vector_enumeration_steps(depth+1, &new_combination, &shortest_vector, shortest_length, checked_points);
+            combination[self.columns()-1-depth] = i;
+            let (candidate_vector, candidate_length) = self.shortest_vector_enumeration_steps(depth+1, combination, &shortest_vector, shortest_length, checked_points);
             if candidate_length < shortest_length && candidate_length != 0. {
                 shortest_vector = candidate_vector;
                 shortest_length = candidate_length;
@@ -67,11 +67,11 @@ impl Lattice {
         let start = self.columns()-basis_number+1;
         let end = self.columns();
         let i = self.columns()-basis_number;
-        for (x_j, j) in combination.iter().zip((start..end).into_iter()).rev() {
+        for j in (start..end).into_iter().rev() {
             let B_j = self.get_gram_schmidt_length(j);
             let mu_ji = self.get_mu(j, i);
-            sum += (x_j.pow(2) as f64)*B_j;
-            M_2 += mu_ji*(*x_j as f64);
+            sum += (combination[j].pow(2) as f64)*B_j;
+            M_2 += mu_ji*(combination[j] as f64);
         }
         let M_1 = ((A-sum)/self.get_gram_schmidt_length(i)).sqrt();
 
